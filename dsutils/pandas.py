09b69eb5ef061eeb3f75a591f8100b50a78eeb67
Henk Griffioen <hgrif@users.noreply.github.com>
2015-09-04 15:19:34 +0200

    Rename package to dsutils

--- dsutils/pandas.py
+++ dsutils/pandas.py
@@ -13,15 +13,16 @@ def dataframe_size(df):
                               df.values.nbytes]) / byte_to_megabyte_factor))
     size['total'] = np.sum(size.values())
     return size
-    
+
+
 def print_full(df):
     """Print the full DataFrame  
-    
     :param df: pandas.DataFrame
     :return None
     
     See: 
     - http://stackoverflow.com/questions/19124601/is-there-a-way-to-pretty-print-the-entire-pandas-series-dataframe
     """
-    with pd.option_context('display.max_rows', len(df), 'display.max_columns', len(df.shape[1])):
+    with pd.option_context('display.max_rows', len(df), 'display.max_columns',
+                           len(df.shape[1])):
         print df
